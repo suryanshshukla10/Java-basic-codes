7cf1f99fd3c02f4e70f6a3d4e40431908ca8863d
suryansh shukla <45881980+suryanshshukla10@users.noreply.github.com>
2020-10-28 13:35:16 +0530

    adding for loop2

---
+++ forLoop2.java
@@ -0,0 +1,12 @@
+public class forLoop2 {
+    public static void main(String[]args){
+        for (int j = 0; j < 4; j++){
+            for (int i = 0; i < j; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
