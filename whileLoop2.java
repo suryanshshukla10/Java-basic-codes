08ac63d29e6e0b0a1bc4dccf624cd42992bd9614
suryansh shukla <45881980+suryanshshukla10@users.noreply.github.com>
2020-10-28 13:37:13 +0530

    adding while loop 2

---
+++ whileLoop2.java
@@ -0,0 +1,13 @@
+public class whileLoop2 {
+    public static void main(String[]args){
+        int i = 0;
+        boolean condition = true;
+        while(condition){
+            System.out.println("**");
+            i++;
+            if (i == 2){condition = false;}
+
+        }
+
+    }
+}
