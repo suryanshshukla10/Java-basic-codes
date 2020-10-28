17a78f5bafb276b401f05efddddcdeeed2b88bc0
suryansh shukla <45881980+suryanshshukla10@users.noreply.github.com>
2020-10-28 13:35:49 +0530

    Adding switch markAnalysis

---
+++ switchMarkAnaly.java
@@ -0,0 +1,35 @@
+public class switchMarkAnaly {
+    public static void main(String[] args) {
+        int marks = 25;
+        switch(marks) {
+            case 80:
+            case 81:
+            case 82:
+            case 83:
+            case 84:
+            case 85:
+            case 86:
+            case 87:
+            case 88:
+            case 89:
+                System.out.println("B Grade. Keep it up");
+                break;
+            case 90:
+            case 91:
+            case 92:
+            case 93:
+            case 94:
+            case 95:
+            case 96:
+            case 97:
+            case 98:
+            case 99:
+            case 100:
+                System.out.println("A Grade. Well Done!");
+                break;
+            default:
+                System.out.println("C Grade. Work Hard");
+        }
+
+    }
+}
