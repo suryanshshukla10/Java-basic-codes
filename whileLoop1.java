fb79f0158fed21073cf8df18c9af5c696c3be4fd
suryansh shukla <45881980+suryanshshukla10@users.noreply.github.com>
2020-10-28 13:36:48 +0530

    Adding while Loop

---
+++ whileLoop1.java
@@ -0,0 +1,12 @@
+public class whileLoop1 {
+    public static void main(String[]args){
+        int i = 0;
+        while (i < 108){
+            System.out.println("HelloWorld");
+            i++;
+        }
+
+    }
+
+
+}
